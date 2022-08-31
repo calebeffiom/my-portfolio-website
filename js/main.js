a82d3d549296a11eb8e90c40e0da5fe04eb49ffd
Caleb <95381786+calebeffiom@users.noreply.github.com>
2022-08-31 04:13:41 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// var navScroll = document.getElementById("colorChange");
+// var sect = document.getElementById("next");
+// function changeColor(){
+//     if (document.body.scrollTop > 350){
+//         navScroll.style.backgroundColor = "rgba(236, 235, 235, 0.829)";
+//     }
+// }
+
+
+// document.addEventListener(scrollY(300),function(){
+//     navScroll.style.backgroundColor = "rgba(236, 235, 235, 0.829)";
+// })
